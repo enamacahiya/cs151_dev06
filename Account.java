e80862bc6e4baf071bab5f2bff8b30e61544494b
enamacahiya <enacjmac@gmail.com>
2024-10-23 21:36:19 -0700

    account class changes

--- Account.java
+++ Account.java
@@ -1,11 +1,27 @@
 package application;
+
+import java.util.*;
 import java.time.LocalDate;
+import javafx.scene.control.TextField;
 
 public class Account {
-	
-	private String acc_name;
-	private LocalDate openingDate;
-	private double openingBalance;
-}
-public Account(
 
+    private String accName;
+    private LocalDate openingDate;
+    private double accBalance;
+
+    public Account(TextField accName, LocalDate openingDate, double accBalance) {
+        this.accName = accName.getText();
+        this.openingDate = openingDate;
+        this.accBalance = accBalance;
+    }
+
+    public ArrayList<String> getAccDetails() {
+        ArrayList<String> temp = new ArrayList<String>();
+        temp.add(accName);
+        temp.add(openingDate.toString());
+        temp.add(Double.toString(accBalance));
+        return temp;
+    }
+
+}

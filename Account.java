69afd25b9b8a72c07fa297dd76795ed401d2bc26
JB-Rose <joshbrose1@gmail.com>
2024-10-23 21:52:32 -0700

    Update Account.java

--- Account.java
+++ Account.java
@@ -2,25 +2,26 @@ package application;
 
 import java.util.*;
 import java.time.LocalDate;
+import javafx.scene.control.TextField;
 
 public class Account {
-
+    
     private String accName;
     private LocalDate openingDate;
     private double accBalance;
-
-    public Account(TextField accName, LocalDate openingDate, double accBalance) {
-        this.accName = accName.getText();
+    
+    public Account(String accName, LocalDate openingDate, double accBalance) {
+        this.accName = accName;
         this.openingDate = openingDate;
         this.accBalance = accBalance;
     }
-
+    
     public ArrayList<String> getAccDetails() {
-        ArrayList<String> temp = new ArrayList<String>();
+    	ArrayList<String> temp = new ArrayList<>();
         temp.add(accName);
         temp.add(openingDate.toString());
         temp.add(Double.toString(accBalance));
         return temp;
     }
-
+    
 }

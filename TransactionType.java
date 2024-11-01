49564687cff89eae601f5d0665192bad8451e883
JB-Rose <joshbrose1@gmail.com>
2024-11-01 12:32:38 -0700

    combined transacitons UI and DAL

---
+++ TransactionType.java
@@ -0,0 +1,22 @@
+package application;
+
+import java.util.ArrayList;
+
+public class TransactionType implements CSVWritable{
+	private String typeName;
+	
+	public TransactionType (String name) {
+		this.typeName = name;
+	}
+	
+	public TransactionType (ArrayList<String> data) {
+		this.typeName = data.removeFirst();
+	}
+	
+	@Override
+	public ArrayList<String> toArrayList() {
+		ArrayList<String> temp = new ArrayList<>();
+		temp.add(typeName);
+		return temp;
+	}
+}
